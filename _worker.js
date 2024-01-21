e60bea88f9bb11c31c30498444e93f96b129472a
Ali-ph <135620449+Ali-ph@users.noreply.github.com>
2024-01-21 12:14:54 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = '64651b6c-5fc0-4006-a440-69a6adc72634';
+let userID = '15dda58c-9689-444d-994a-0a881ce9e2b8';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
